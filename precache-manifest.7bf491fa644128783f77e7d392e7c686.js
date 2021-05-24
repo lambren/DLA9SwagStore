badc4de4357a0a2f8a1260c6cbdb511b8b90058b
lambren <lambren@gmail.com>
2021-05-23 22:23:41 -0700

    Updates

---
+++ precache-manifest.7bf491fa644128783f77e7d392e7c686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9596903a9aa486e98358f4f50fb4ab3a",
+    "url": "/DLA9SwagStore/index.html"
+  },
+  {
+    "revision": "a1d88798de2543873b2a",
+    "url": "/DLA9SwagStore/static/css/main.408db1a0.chunk.css"
+  },
+  {
+    "revision": "f0266b82621aa94ca6ae",
+    "url": "/DLA9SwagStore/static/js/2.57cb00c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DLA9SwagStore/static/js/2.57cb00c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d88798de2543873b2a",
+    "url": "/DLA9SwagStore/static/js/main.44b67049.chunk.js"
+  },
+  {
+    "revision": "e878701ae46390dad4ce",
+    "url": "/DLA9SwagStore/static/js/runtime-main.6baa1702.js"
+  },
+  {
+    "revision": "2821557b9d99fcca064e4b7ea54b138a",
+    "url": "/DLA9SwagStore/static/media/Amazon.2821557b.jpg"
+  }
+]);
